6d93ac860e6ceb5b16af3d9cab2cba9727518124
Kacper Grabowski <enjoying2003@gmail.com>
2025-02-27 22:09:36 +0100

    Medium rebuild

--- miniChat/main.py
+++ miniChat/main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, make_response, session
 import sqlite3 as s
-from os import urandom
+from os import urandom, _exit
 from flask_socketio import SocketIO, emit
 
 conn = s.connect("text.db")
@@ -14,6 +14,46 @@ app = Flask(__name__)
 app.secret_key = urandom(256)
 
 socketio = SocketIO(app)
+
+def get_messages():
+    data = None
+    conn = s.connect("text.db")
+    cur = conn.cursor()
+    sql = "SELECT uname, text FROM chats"
+    cur.execute(sql)
+    data = cur.fetchall()
+    conn.close()
+    return data
+
+@socketio.on("message sent")
+def message_sent(data):
+    if session.get('username'):
+        conn = s.connect("text.db")
+        cur = conn.cursor()
+        uname = session['username']
+        sql = "INSERT INTO chats(uname,text) VALUES (?, ?)"
+        cur.execute(sql, (uname, data['data']))
+        conn.commit()
+        conn.close()
+        data = get_messages()
+        print(data)
+        socketio.emit("messages_load", data)
+    else:
+        socketio.emit("redirect", {"url": "/"})
+
+@socketio.on("connection made")
+def connected(data):
+    print("Connection made")
+
+@app.get("/stop")
+def stop():
+    conn = s.connect("text.db")
+    cur = conn.cursor()
+    cur.execute("DELETE FROM chats")
+    conn.commit()
+    conn.close()
+    _exit(0)
+    return "That\'s it folks..."
 
 @app.route("/", methods=["GET", "POST"])
 def register():
@@ -32,35 +72,17 @@ def register():
             session['username'] = request.form['uname']
             return resp
     else:
-        return 500
+        return make_response(redirect(url_for("register")))
 
-@app.route("/chat", methods=["GET", "POST"])
+@app.route("/chat", methods=["GET"])
 def chat():
-    conn = s.connect("text.db")
-    cur = conn.cursor()
     if request.method == "GET":
-        sql = "SELECT uname, text FROM chats"
-        cur.execute(sql)
-        rows = cur.fetchall()
-        conn.close()
-        return render_template("chat.html", rows=rows)
-    elif request.method == "POST":
-        if request.form["text"] != "":
-            try:
-                uname = session['username']
-                sql = "INSERT INTO chats(uname,text) VALUES (?, ?)"
-                cur.execute(sql, (uname, request.form["text"]))
-                conn.commit()
-                cur.execute("SELECT uname, text FROM chats")
-                query_result = cur.fetchall()
-                conn.close()
-                socketio.emit("new message", query_result, broadcast=True)
-                return "", 204
-            except:
-                return redirect(url_for("register"))
+        if session.get("username"):
+            socketio.emit("messages_load", get_messages())
+            return render_template("chat.html")
         else:
-            return redirect(url_for("chat"))
+            return redirect(url_for("register"))
 
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", debug=True)
+    socketio.run(app, host="0.0.0.0", debug=False)
